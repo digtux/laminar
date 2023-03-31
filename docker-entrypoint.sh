690db036902b8600ae209f129a31b09a058c66bd
David Stark <david@starkers.org>
2023-03-31 15:59:11 +0100

    user should have permission to write to ~/.ssh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -14,7 +14,7 @@ if [ "${1:0:1}" = '-' ]; then
         ssh-keyscan -t rsa "${GITHOST:-github.com}" 2>/dev/null \
         > ~/.ssh/known_hosts
 
-  chmod 0640 ~/.ssh
+  chmod 0700 ~/.ssh
 
   eval "$(ssh-agent)"
   ssh-add "${SSH_KEY:-~/.ssh/id_rsa}"
